9444885e629c23508175cc6a80254c01a2b0fd7a
PedroFreitasFanatee <pedropifre@gmail.com>
2022-06-24 02:18:31 -0300

    PowerUps Completed

--- Assets/Scripts/Itens/ItemCollactableBase.cs
+++ Assets/Scripts/Itens/ItemCollactableBase.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ItemCollactableBase : MonoBehaviour
 {
     public string compareTag = "Player";
     public float timeToHide=3;
     public GameObject graphicItem;
+    public TextMeshPro graphicText;
     public ParticleSystem particleSystem;
 
     [Header("Sounds")]
@@ -29,6 +31,7 @@ public class ItemCollactableBase : MonoBehaviour
     protected virtual void HideItens()
     {
         if (graphicItem != null) graphicItem.SetActive(false);
+        if (graphicText != null) graphicText.text = "";
         Invoke(nameof(HideObject), timeToHide);
     }
     protected virtual void Collect() 
